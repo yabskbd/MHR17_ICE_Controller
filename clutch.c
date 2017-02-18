92e6274dfa20c42fdd48dc27339020199fbd9977
Andrew Stenberg <andrewstenberg2@gmail.com>
2017-02-18 17:10:21 -0500

    added framework for polling CAN bus

--- clutch.c
+++ clutch.c
@@ -3,6 +3,23 @@
 
 int main(){
 
+	// setup
+	can_init(0x0);
+
+	U8 data;
+	st_cmd_t recieve;
+
+	recieve.pt_data = data;
+	recieve.cmd = CMD_RX;
+
+	// execution loop
+	while(1) {
+
+		while(can_cmd(recieve)==CAN_CMD_ACCEPTED) {
+
+		}
+	}
+
     return 0;
 
 }
