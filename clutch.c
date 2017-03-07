b6bee229d9010ab64b54908788169a0e500e6b26
Andrew Stenberg <andrewstenberg2@gmail.com>
2017-03-06 21:31:51 -0500

    added functionality for sending characters over UART

--- clutch.c
+++ clutch.c
@@ -1,12 +1,38 @@
+/************************************************
+************************************************/
+
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "config.h"
 #include "libraries/lib_mcu/can/can_lib.h"
 #include "libraries/lib_mcu/uart/uart_lib.h"
 
-// OC0A is the pin outputtng the pwm signal
-void pwm_init() {
+// setup and initialize UART for debugging messages
+void serial_init(unsigned int baud_rate)
+{
+	UCSR0C |= (0<<UMSEL0);				//asynchronous operation
+	UCSR0A |= (0<<U2X0);				//normal asynchronous mode
+	UBRR0H |= (unsigned char) (baud_rate>>8);	//set the baud rate (high register)
+	UBRR0L |= (unsigned char) baud_rate;		//set the baud rate (low register)
+	UCSR0C |= (0<<UPM01)|(0<<UPM00);		//no parity bits
+	UCSR0C |= (0<<USBS0);				//one stop bit
+	UCSR0C |= (0<<UCSZ02)|(1<<UCSZ01)|(1<<UCSZ01);	//8-bit character size
+	UCSR0B |= (1<<TXEN0);				//enable transmitter
+	return;
+}
 
+// function for sending debug messages over serial bus
+void send_byte(unsigned char data)
+{
+	// wait for transmit buffer to be empty
+	while(!(UCSR0A & (1<<UDRE0)));
+	UDR0 = data;	// write to transmit buffer
+}
+
+// OC0A is the pin outputtng the pwm signal
+void pwm_init()
+{
 	// set pwm to fast PWM mode, non-inverting compare mode, and prescalar to 8
 	TCCR0A |= (1<<WGM00)|(1<<WGM01)|(1<<COM0A1)|(1<<CS01);
 
@@ -20,53 +46,30 @@ void pwm_init() {
 	return;
 }
 
-void adc_init() {
-
+void adc_init()
+{
 	// turn on adc
-    
 	ADMUX |= (1 << REFS0); //SET Voltage ref to internal VIN
     	ADMUX |= (1 << ADLAR); //LEFT SHIFT so we just have to READ ADCH reg for data
     	//DEFAULT MUX is set to ADC0
-    	ADCSRA |= (1<<ADEN); //ADC Enabled
+    	ADCSRA |= (1<<ADEN);   //ADC Enabled
 
 	return;
 }
 
-//void USART_Init(unsigned int baud){
-//
-//    //SET baud rate
-//    UBRR0H = (unsigned char) (baud >> 8);
-//    UBRR0L = (unsigned char) baud;
-//
-//    //set Frame format: 8data, no parity & 2 stop bits
-//    UCSR0C = (0 << UMSEL0) | (0<<UPM0) | (1 << USBS0) | (3 << UCSZ0);
-//    //Enable Transmit and reciever
-//    UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-//
-//}
-//
-//void USART0_Transmit(uint8_t data){
-//
-//    /*Wait for empty transmit buffer*/
-//    while(!(UCSR0A & (1<<UDRE0)));
-//    UCSR0B &= ~(1<<TXB80);
-//    //Put data 
-//    UDR0 = data;
-//
-//}
-
 int counter;
-ISR(INT0_vect) {
+ISR(INT0_vect)
+{
 	counter++;
 }
 
-int main(void){
-
+void main(void)
+{
     	// setup timer for PWM to control motor 
 	pwm_init();
     	adc_init();
-    	//Init Serial UART
-    	while(uart_init(UART_BAUDRATE,0x0680)!=1);
+	serial_init(9600);
+	send_byte('a');
 
     	// incremental encoder counter setup
 	DDRD  |= (1<<PD0);              // set PD0 as intput, used for INT0
@@ -75,13 +78,10 @@ int main(void){
 	sei(); 				// enable global inturrupts
 	
 	// execution loop
-	while(1) {
-        
-        	uart_put_string("GO Packards");
-        	ADCSRA |= (1<<ADSC);
-        	while(ADCSRA & 0x40);
-        	if(ADCH > 10){
-            		OCR0A = (ADCH)-12;
-        	}
+	while(1)
+       	{
+
 	}
+
+	return;
 }
