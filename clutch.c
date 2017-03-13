cf3483f4b368c1d9856659578d9a650367e58348
Andrew Stenberg <andrewstenberg2@gmail.com>
2017-03-12 22:22:43 -0400

    fixed tab spacing

--- clutch.c
+++ clutch.c
@@ -16,65 +16,65 @@
 // setup and initialize UART for debugging messages
 void serial_init()
 {
-	UCSR0C |= (0<<UMSEL0);				//asynchronous operation
-	UCSR0A |= (0<<U2X0);				//normal asynchronous mode
-	UBRR0H |= (unsigned char) (BAUD_PRESCALE>>8);	//set the baud rate (high register)
-	UBRR0L |= (unsigned char) BAUD_PRESCALE;	//set the baud rate (low register)
-	UCSR0C |= (0<<UPM01)|(0<<UPM00);		//no parity bits
-	UCSR0C |= (0<<USBS0);				//one stop bit
-	UCSR0C |= (0<<UCSZ02)|(1<<UCSZ01)|(1<<UCSZ01);	//8-bit character size
-	UCSR0B |= (1<<TXEN0);				//enable transmitter
-	return;
+    UCSR0C |= (0<<UMSEL0);				//asynchronous operation
+    UCSR0A |= (0<<U2X0);				//normal asynchronous mode
+    UBRR0H |= (unsigned char) (BAUD_PRESCALE>>8);	//set the baud rate (high register)
+    UBRR0L |= (unsigned char) BAUD_PRESCALE;	//set the baud rate (low register)
+    UCSR0C |= (0<<UPM01)|(0<<UPM00);		//no parity bits
+    UCSR0C |= (0<<USBS0);				//one stop bit
+    UCSR0C |= (0<<UCSZ02)|(1<<UCSZ01)|(1<<UCSZ01);	//8-bit character size
+    UCSR0B |= (1<<TXEN0);				//enable transmitter
+    return;
 }
 
 // functions for sending debug messages over serial bus
 void send_char(char data)
 {
 	// wait for transmit buffer to be empty
-	while(!(UCSR0A & (1<<UDRE0)));
-	UDR0 = data;	// write to transmit buffer
-	return;
+    while(!(UCSR0A & (1<<UDRE0)));
+    UDR0 = data;	// write to transmit buffer
+    return;
 }
 
 void send_message(char* message)
 {
 	//while there are still characters to send
-	while(*message |= '\0')
-	{
-		send_char(*message);
-		message++;
-	}
-	return;
+    while(*message |= '\0')
+    {
+        send_char(*message);
+        message++;
+    }
+    return;
 }
 
 // initialize pwm for throttle
 void send_int(U16 data)
 {
-	// wait for transmit buffer to be empty
-	while(!(UCSR0A & (1<<UDRE0)));
+    // wait for transmit buffer to be empty
+    while(!(UCSR0A & (1<<UDRE0)));
     char str[7];
     sprintf(str,"%d",data);
-	send_message(str);
+    send_message(str);
     return;
 }
 
 // OC1A is the pin outputtng the pwm signal
 void pwm_init()
 {
-	DDRB |= (1<<PB5);	// set as output
-	TCCR1B |= (0<<CS12)|(1<<CS11)|(0<<CS10);	//set prescalar
-	TCCR1A |= (1<<COM1A1)|(0<<COM1A0);	//set compare output mode
-	TCCR1A |= (1<<WGM11)|(0<<WGM10);	//set output mode
-	TCCR1B |= (1<<WGM13)|(1<<WGM12);
-	ICR1 = 39999;
-	OCR1A = 20000;
-	return;
+    DDRB |= (1<<PB5);	// set as output
+    TCCR1B |= (0<<CS12)|(1<<CS11)|(0<<CS10);	//set prescalar
+    TCCR1A |= (1<<COM1A1)|(0<<COM1A0);	//set compare output mode
+    TCCR1A |= (1<<WGM11)|(0<<WGM10);	//set output mode
+    TCCR1B |= (1<<WGM13)|(1<<WGM12);
+    ICR1 = 39999;
+    OCR1A = 20000;
+    return;
 }
 
 // set duty cycle of throttle
 void set_duty(unsigned int  duty)
 {
-	U16 diff = MIN_THROTTLE - MAX_THROTTLE;
+    U16 diff = MIN_THROTTLE - MAX_THROTTLE;
     send_message("Diff between max and min \t");
     send_int(diff);
     U16 percent = (duty*100)/255;
@@ -89,24 +89,24 @@ void set_duty(unsigned int  duty)
     OCR1A = final;
     send_message("OCR1A: \t");
     send_int(OCR1A);
-	return;
+    return;
 }
 
 void adc_init()
 {
-	// turn on adc
-	ADMUX |= (1<<REFS0); //SET Voltage ref to internal VIN
-    	ADMUX |= (1<<ADLAR); //LEFT SHIFT so we just have to READ ADCH reg for data
-    	//DEFAULT MUX is set to ADC0
-    	ADCSRA |= (1<<ADEN);   //ADC Enabled
-	return;
+    // turn on adc
+    ADMUX |= (1<<REFS0); //SET Voltage ref to internal VIN
+    ADMUX |= (1<<ADLAR); //LEFT SHIFT so we just have to READ ADCH reg for data
+    //DEFAULT MUX is set to ADC0
+    ADCSRA |= (1<<ADEN);   //ADC Enabled
+    return;
 }
 
 
 U16 counter = 0;
 ISR(INT0_vect)
 {
-	counter++;
+    counter++;
     if(counter >= 16)
     {
         counter = 0;
@@ -117,53 +117,53 @@ ISR(INT0_vect)
 
 void main(void)
 {
-    	// setup timer for PWM to control motor 
+    // setup timer for PWM to control motor 
     adc_init();
-	pwm_init();
-	serial_init();
+    pwm_init();
+    serial_init();
 	
-	// pin for turning one motor
-	DDRA |= (1<<PA1); //Set reg to be an ouput
-	PORTA &= ~(1<<PA1); //Setting bit high or low 
+    // pin for turning one motor
+    DDRA |= (1<<PA1); //Set reg to be an ouput
+    PORTA &= ~(1<<PA1); //Setting bit high or low 
 
-	//initialize can
-	//while(can_init(0) != 1);
-	//st_cmd_t can_message;
-	//can_id_t can_id;
+    //initialize can
+    //while(can_init(0) != 1);
+    //st_cmd_t can_message;
+    //can_id_t can_id;
 
     // incremental encoder counter setup
-	DDRD  &= ~(1<<PD0);              // set PD0 as intput, used for INT0
-	PORTD |= (1<<PD0);	        // enable pullup resistor
+    DDRD  &= ~(1<<PD0);              // set PD0 as intput, used for INT0
+    PORTD |= (1<<PD0);	        // enable pullup resistor
     EIMSK |= (1 << INT0);
-	EICRA |= (1<<ISC00)|(1<<ISC01); // set interupt to trigger on rising edge of INT0
-	sei(); 				// enable global inturrupts
+    EICRA |= (1<<ISC00)|(1<<ISC01); // set interupt to trigger on rising edge of INT0
+    sei(); 				// enable global inturrupts
 	
-	//U16 test = 255;
+    //U16 test = 255;
     //send_int(OCR1A);
     //send_char('\n');
     //send_message("Value of OCR1A at call");
-	set_duty(0);	
+    set_duty(0);	
 	
-	// execution loop
-	while(1){
-		// load in message
-		//can_message.cmd = CMD_RX_DATA;
-		//while(can_cmd(&can_message) != CAN_CMD_ACCEPTED);
-		//while(can_get_status(&can_message) != CAN_STATUS_COMPLETED);
-       //send_int(PIND & (1<<PIND0));
+    // execution loop
+    while(1){
+        // load in message
+        //can_message.cmd = CMD_RX_DATA;
+        //while(can_cmd(&can_message) != CAN_CMD_ACCEPTED);
+        //while(can_get_status(&can_message) != CAN_STATUS_COMPLETED);
+        //send_int(PIND & (1<<PIND0));
         send_int(counter);
         send_char('\t');
-		// check id
-		/*switch(can_message.id.std)
-		{
-			case THROTTLE_ID:
-				//set_duty(can_message.pt_data);
-				break;
-			case CLUTCH_ID:
-				break;
-			default:
-				break;
-		}*/
-	}
-	return;
+        // check id
+        /*switch(can_message.id.std)
+        {
+            case THROTTLE_ID:
+                //set_duty(can_message.pt_data);
+                break;
+            case CLUTCH_ID:
+                break;
+            default:
+                break;
+        }*/
+    }
+    return;
 }
